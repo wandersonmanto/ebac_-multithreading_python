0bf81b2df9361cafd92260877630d9bfc1921d92
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2023-07-03 10:46:28 -0300

    Alterando user-agent, para respeitar novo formato do site

--- multithreading.py
+++ multithreading.py
@@ -8,7 +8,7 @@ import concurrent.futures
 from bs4 import BeautifulSoup
 
 # global headers to be used for requests
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
 
 MAX_THREADS = 10
 
