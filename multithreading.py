d74be0a933d32d4de2c4cbb6a50236c9d91c060c
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2023-07-03 10:38:54 -0300

    Alterando estrutura do Scraping do site.

--- multithreading.py
+++ multithreading.py
@@ -41,8 +41,8 @@ def extract_movie_details(movie_link):
 
 
 def extract_movies(soup):
-    movies_table = soup.find('table', attrs={'data-caller-name': 'chart-moviemeter'}).find('tbody')
-    movies_table_rows = movies_table.find_all('tr')
+    movies_table = soup.find('div', attrs={'data-testid': 'chart-layout-main-column'}).find('ul')
+    movies_table_rows = movies_table.find_all('li')
     movie_links = ['https://imdb.com' + movie.find('a')['href'] for movie in movies_table_rows]
 
     threads = min(MAX_THREADS, len(movie_links))
