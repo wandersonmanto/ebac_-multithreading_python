534658f4bd4a69a9c66597877e89a1f6806bcb1c
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2023-08-17 14:16:33 -0300

    Atualização nova do IMDB
    Ajuste realizado 08/17

--- multithreading.py
+++ multithreading.py
@@ -22,7 +22,7 @@ def extract_movie_details(movie_link):
         title = None
         date = None
 
-        movie_data = movie_soup.find('div', attrs={'class': 'sc-385ac629-3 kRUqXl'})
+        movie_data = movie_soup.find('div', attrs={'class': 'sc-e226b0e3-3 jJsEuz'})
         if movie_data is not None:
             title = movie_data.find('h1').get_text()
             date = movie_data.find('a', attrs={'class': 'ipc-link ipc-link--baseAlt ipc-link--inherit-color'}).get_text().strip()
@@ -30,8 +30,8 @@ def extract_movie_details(movie_link):
         rating = movie_soup.find('span', attrs={'class': 'sc-bde20123-1 iZlgcd'}).get_text() if movie_soup.find(
             'span', attrs={'class': 'sc-bde20123-1 iZlgcd'}) else None
 
-        plot_text = movie_soup.find('span', attrs={'class': 'sc-6a7933c5-1 fPmRoa'}).get_text().strip() if movie_soup.find(
-            'span', attrs={'class': 'sc-6a7933c5-1 fPmRoa'}) else None
+        plot_text = movie_soup.find('span', attrs={'class': 'sc-466bb6c-0 kJJttH'}).get_text().strip() if movie_soup.find(
+            'span', attrs={'class': 'sc-466bb6c-0 kJJttH'}) else None
 
         with open('movies.csv', mode='a') as file:
             movie_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
