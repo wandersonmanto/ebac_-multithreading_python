a3ef08e05076c096d742122f26a54fb39d8c1006
Bruno Saldanha <brunosaldanhamarinho@gmail.com>
2024-07-01 18:35:44 -0300

    :pencil: Troca de div para section
    Essa atualização agora foi focada em substituir a forma que o código lê a página. Ele vai procurar a seção antes, e depois de ter acesso a seção, ele vai buscar pela div em específico que possua os elementos hierárquicos para a composição da página.

--- multithreading.py
+++ multithreading.py
@@ -3,8 +3,6 @@ import time
 import csv
 import random
 import concurrent.futures
-
-
 from bs4 import BeautifulSoup
 
 # global headers to be used for requests
@@ -12,46 +10,50 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 
 MAX_THREADS = 10
 
-
 def extract_movie_details(movie_link):
     time.sleep(random.uniform(0, 0.2))
-    response = BeautifulSoup(requests.get(movie_link, headers=headers).content, 'html.parser')
-    movie_soup = response
+    response = requests.get(movie_link, headers=headers)
+    movie_soup = BeautifulSoup(response.content, 'html.parser')
 
     if movie_soup is not None:
         title = None
         date = None
-        # ajustar o trecho abaixo de acordo com o site de um filme, exemplo : https://www.imdb.com/title/tt15398776/?ref_=chtmvm_t_1, usar inspecionar elemento para definir os elementos.
-        movie_data = movie_soup.find('div', attrs={'class': 'sc-1f50b7c-0 PUxFE'})
-        #print(movie_data)
-        if movie_data is not None:
-            # h1 deve ser o título do nome do filme
-            titlecap = movie_data.find('h1')
-            # print (titlecap)
-            if titlecap is not None:
-                title = movie_data.find('span').get_text()
-                # print(title)
-            # date deve ser apenas a classe que representa o ano.
-            date = movie_data.find('a', attrs={'class': 'ipc-link ipc-link--baseAlt ipc-link--inherit-color'}).get_text().strip()
-            #print(date)
-        # rating é a nota do filme, por exemplo, 8.6.
-        rating = movie_soup.find('span', attrs={'sc-eb51e184-1 cxhhrI'}).get_text() if movie_soup.find(
-            'span', attrs={'sc-eb51e184-1 cxhhrI'}) else None
-        #print(rating)
-        # plot é o texto de sinopse do filme
-        plot_text = movie_soup.find('span', attrs={'data-testid': 'plot-xs_to_m'}).get_text().strip() if movie_soup.find(
-            'span', attrs={'data-testid': 'plot-xs_to_m'}) else None
-        #print(plot_text)
-
-        with open('movies.csv', mode='a') as file:
-            movie_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            if all([title, date, rating, plot_text]):
-                print(title, date, rating, plot_text)
-                movie_writer.writerow([title, date, rating, plot_text])
-
+        
+        # Encontrando a seção específica
+        page_section = movie_soup.find('section', attrs={'class': 'ipc-page-section'})
+        
+        if page_section is not None:
+            # Encontrando todas as divs dentro da seção
+            divs = page_section.find_all('div', recursive=False)
+            
+            if len(divs) > 1:
+                target_div = divs[1]
+                
+                # Encontrando o título do filme
+                title_tag = target_div.find('h1')
+                if title_tag:
+                    title = title_tag.find('span').get_text()
+                
+                # Encontrando a data de lançamento
+                date_tag = target_div.find('a', href=lambda href: href and 'releaseinfo' in href)
+                if date_tag:
+                    date = date_tag.get_text().strip()
+                
+                # Encontrando a classificação do filme
+                rating_tag = movie_soup.find('div', attrs={'data-testid': 'hero-rating-bar__aggregate-rating__score'})
+                rating = rating_tag.get_text() if rating_tag else None
+                
+                # Encontrando a sinopse do filme
+                plot_tag = movie_soup.find('span', attrs={'data-testid': 'plot-xs_to_m'})
+                plot_text = plot_tag.get_text().strip() if plot_tag else None
+                
+                with open('movies.csv', mode='a', newline='', encoding='utf-8') as file:
+                    movie_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    if all([title, date, rating, plot_text]):
+                        print(title, date, rating, plot_text)
+                        movie_writer.writerow([title, date, rating, plot_text])
 
 def extract_movies(soup):
-    # aqui são configurações de hierarquia da página, do ponto de encontro do filme até sua divisão e organização em elementos.
     movies_table = soup.find('div', attrs={'data-testid': 'chart-layout-main-column'}).find('ul')
     movies_table_rows = movies_table.find_all('li')
     movie_links = ['https://imdb.com' + movie.find('a')['href'] for movie in movies_table_rows]
@@ -59,7 +61,6 @@ def extract_movies(soup):
     threads = min(MAX_THREADS, len(movie_links))
     with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
         executor.map(extract_movie_details, movie_links)
-
 
 def main():
     start_time = time.time()
@@ -74,7 +75,6 @@ def main():
 
     end_time = time.time()
     print('Total time taken: ', end_time - start_time)
-
 
 if __name__ == '__main__':
     main()
